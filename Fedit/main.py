c427e002ac820c55937db374911dd576c22fff25
Pythonic456 <noah.horler@gmail.com>
2021-01-09 17:53:19 +0000

    Add text editor frame, make it expandable

--- Fedit/main.py
+++ Fedit/main.py
@@ -7,8 +7,12 @@ version = '0.0.0.1'
 
 root = tktools.Window('Fedit ('+str(version)+')').window_raw()
 
-widget_maintextedit = tktools.TextEditor(root).widget_raw()
-widget_maintextedit.pack()
+frame_maintextedit = tk.Frame(root)
+
+widget_maintextedit = tktools.TextEditor(frame_maintextedit).widget_raw()
+widget_maintextedit.pack(expand=1, fill='both')
+
+frame_maintextedit.pack(expand=1, fill='both')
 
 root.mainloop()
 print('Exiting')
