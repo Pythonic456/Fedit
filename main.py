0849c0bea6a529a1b94a37d370780aab7b2b6f24
Pythonic456 <noah.horler@gmail.com>
2021-01-28 10:00:11 +0000

    Fix bugs

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from tktools import tk, tkfd
 
 version = '0.0.0.1'
 ftypes = [
+    ('All files', '*'),
     ('Python code files', '*.py'), 
     ('Perl code files', '*.pl;*.pm'),  # semicolon trick
     ('Java code files', '*.java'), 
     ('C++ code files', '*.cpp;*.h'),   # semicolon trick
-    ('Text files', '*.txt'), 
-    ('All files', '*'), 
+    ('Text files', '*.txt'),  
 ]
+initial_dir = os.path.expanduser('~')
 
 print('Starting Fedit '+version)
 
@@ -23,17 +24,31 @@ widget_maintextedit = tktools.TextEditor(frame_maintextedit)
 menubar = tktools.MenuBar(frame_menubar)
 
 
+def verify_f_name(f_name):
+    if f_name in [None, ()]:
+        return True
+    return False
+
 def open_file():
-    global ftypes, widget_maintextedit
-    f_name = tkfd.askopenfilename(filetypes=ftypes, initialdir=os.path.expanduser('~'))
+    global ftypes, widget_maintextedit, initial_dir
+    f_name = tkfd.askopenfilename(filetypes=ftypes, initialdir=initial_dir)
+    if verify_f_name(f_name):
+        return
+    print(f_name)
+    initial_dir = f_name.split('/')
+    initial_dir.pop(-1)
+    initial_dir = '/'.join(initial_dir)
     f_text = open(f_name, 'r').read()
     widget_maintextedit.set_text(text=f_text)
 
 def save_file():
-    global ftypes, widget_maintextedit
-    f_name = tkfd.asksaveasfilename(filetypes=ftypes, initialdir=os.path.expanduser('~'))
-    if f_name is None:
+    global ftypes, widget_maintextedit, initial_dir
+    f_name = tkfd.asksaveasfilename(filetypes=ftypes, initialdir=initial_dir)
+    if not verify_f_name(f_name):
         return
+    initial_dir = f_name.split('/')
+    initial_dir.pop(-1)
+    initial_dir = '/'.join(initial_dir)
     text = widget_maintextedit.get_text(0.0)
     f_obj = open(f_name, 'w')
     f_obj.write(text)
