7b56ec9220d04d35fff03d67ce9974c63e32f98a
Pythonic456 <noah.horler@gmail.com>
2021-04-10 11:13:05 +0100

    Merged @Painadath Fedit, and changed setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ import os
 import sys
 
 if(len(sys.argv)>=1 and sys.argv[1] == 'install'):
-    os.system('sudo rm -r /usr/share/fedit &&  sudo rm /usr/share/applications/fedit.desktop')
-    os.system('sudo mkdir /usr/share/fedit')
+    os.system('rm -r /usr/share/fedit && rm /usr/share/applications/fedit.desktop')
+    os.system('mkdir /usr/share/fedit')
     setup(name='Fedit',
       version='0.0.1.6',
       description='A simple text editor in python with tkinter',
       author='Pythonic456',
       data_files=[
-        ('/usr/share/applications', ['fedit.desktop']),
-        ('/usr/share/fedit/',['fedit.py']),
-        ('/usr/share/fedit/',['fedit.png']),
+        ('/usr/share/applications', ['data/fedit.desktop']),
+        ('/usr/share/fedit/',['main.py']),
+        ('/usr/share/fedit/',['data/fedit.png']),
         ('/usr/share/fedit/',['tktools.py'])
         ]
     )
 if(len(sys.argv)>=1 and sys.argv[1] == 'remove'):
-    os.system('sudo rm -r /usr/share/fedit &&  sudo rm /usr/share/applications/fedit.desktop')
+    os.system('rm -r /usr/share/fedit && rm /usr/share/applications/fedit.desktop')
