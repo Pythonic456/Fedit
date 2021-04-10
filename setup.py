913f298923590cab21f543d3f75b2b9ec780efb4
Painadath <74921727+Painadath@users.noreply.github.com>
2021-04-10 09:29:06 +0530

    Add files via upload
    Add a full functional setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
-params = {}
-params['scripts'] = ['bin/fedit']
-setup(name='Fedit',
-      version='0.0.1.5',
+import os
+import sys
+
+if(len(sys.argv)>=1 and sys.argv[1] == 'install'):
+    os.system('sudo rm -r /usr/share/fedit &&  sudo rm /usr/share/applications/fedit.desktop')
+    os.system('sudo mkdir /usr/share/fedit')
+    setup(name='Fedit',
+      version='0.0.1.6',
       description='A simple text editor in python with tkinter',
       author='Pythonic456',
-      packages=['tktools'],
       data_files=[
-        ('/usr/share/pixmaps', ['data/fedit.png']),
-        ('/usr/share/applications', ['data/fedit.desktop'])
-        ],
-      **params
- )
+        ('/usr/share/applications', ['fedit/fedit.desktop']),
+        ('/usr/share/fedit/',['fedit/fedit.py']),
+        ('/usr/share/fedit/',['fedit/fedit.png']),
+        ('/usr/share/fedit/',['fedit/tktools.py'])
+        ]
+    )
+if(len(sys.argv)>=1 and sys.argv[1] == 'remove'):
+    os.system('sudo rm -r /usr/share/fedit &&  sudo rm /usr/share/applications/fedit.desktop')
